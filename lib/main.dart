087eb024c7db621f9cace72cf303de40660970db
AdityaChaudhary18 <sanjords@gmail.com>
2021-09-16 21:39:46 +0530

    SVG rendering issue fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:dose_care/router.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sizer/sizer.dart';
 import 'package:flutter/services.dart';
@@ -22,6 +24,18 @@ class MyApp extends StatelessWidget {
     return Sizer(
       builder: (BuildContext context, Orientation orientation,
           DeviceType deviceType) {
+        Future.wait([
+          precachePicture(
+            ExactAssetPicture(
+                SvgPicture.svgStringDecoder, 'images/medicalIntro.svg'),
+            null,
+          ),
+          precachePicture(
+            ExactAssetPicture(
+                SvgPicture.svgStringDecoder, 'images/bloodIntro.svg'),
+            null,
+          ),
+        ]);
         return MaterialApp(
           debugShowCheckedModeBanner: false,
           title: "Dose Tracker",
